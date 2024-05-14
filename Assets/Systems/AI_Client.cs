6fd208d3bf310b12cff50d0e51965f640002611b
Irene-ProgrammerWoman <is.sinisterra71@gmail.com>
2024-05-14 21:09:07 +0200

    Client AI implementation
    [ADD] Client GameObject
    [ADD] Client script

---
+++ Assets/Systems/AI_Client.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Client : MonoBehaviour
+{
+    public enum ClientState
+    {
+        EnterShopState = 1,
+        WaitingState = 2,
+        RageState = 3,
+        ExitShopState = 4,
+    }
+
+    [SerializeField] float movSpeed = 50f;
+    [SerializeField] float minWait = 45f;
+    [SerializeField] float maxWait = 96f;
+
+    private ClientState currentState;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        currentState = ClientState.EnterShopState;
+        ManageNewState((int)currentState);
+    }
+
+    private void ManageNewState(int state)
+    { 
+        switch (state)
+        {
+            case (int)ClientState.EnterShopState:
+                rb.AddForce(Vector2.left * movSpeed);
+                break;
+
+            case (int)ClientState.WaitingState:
+                StartCoroutine(Wait());
+                break;
+
+            case (int)ClientState.RageState:
+                Rage();
+                break;
+
+            case (int)ClientState.ExitShopState:
+                ExitShop();
+                break;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        rb.velocity = Vector2.zero;
+        currentState = ClientState.WaitingState;
+        ManageNewState((int)currentState);
+    }
+
+    IEnumerator Wait() 
+    {
+        float delay = Random.Range(minWait, maxWait);
+        yield return new WaitForSeconds(delay);
+        currentState = ClientState.RageState;
+        ManageNewState((int)currentState);
+    }
+
+    private void Rage()
+    {
+        Debug.Log("RageState");
+        //Sprite fills with red anim
+        //Get mad standing still anim
+        //Yell
+        currentState = ClientState.ExitShopState;
+        ManageNewState((int)currentState);
+    }
+
+    private void ExitShop() 
+    {
+        Debug.Log("ExitState");
+        //Move right
+        //Destroy once it is offscreen
+    }
+
+
+    public int GetClientState()
+    {
+        return (int)currentState;
+    }
+
+
+    
+}
